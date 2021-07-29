644b16e58378fc28d43888752ae1fa705515d035
macong0503 <1512346520@qq.com>
2021-07-29 20:04:21 +0800

    Create StudentException.java

---
+++ Test/HomeWork/StudentException.java
@@ -0,0 +1,10 @@
+package com.hqyj.mc.Test.HomeWork;
+
+public class StudentException extends Exception{
+    public StudentException() {
+    }
+
+    public StudentException(String message) {
+        super(message);
+    }
+}

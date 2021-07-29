cb4d35059d4865e96bc00ba2534575b3e8fd5fa0
macong0503 <1512346520@qq.com>
2021-07-29 20:04:17 +0800

    Create Test.java

---
+++ Test/HomeWork/Test.java
@@ -0,0 +1,10 @@
+package com.hqyj.mc.Test.HomeWork;
+
+public class Test {
+    public static void main(String[] args) throws NoSuchFieldException {
+        Student stu = new Student();
+        stu.setScore(-5);
+    }
+
+
+}
